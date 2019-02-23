149af09d3842ba9109c6c60936ebac6341ed3a83
John Feigel <32643682+FeigelJanos@users.noreply.github.com>
2019-02-23 11:41:34 +0100

    Update TelCheck.js

--- TelCheck.js
+++ TelCheck.js
@@ -1,4 +1,23 @@
 function telephoneCheck(str) {
-
-  return true;
+  let phoneNum=str;
+  let pattern1=/^(1)( |\()\d{3}( |\)|-)\d{3}( |-)\d{4}/;
+  let pattern2/\d{10}/;
+  let pattern3/v/;
+  let patern4/v/
+  
+const phoneNumArr=str.split("");
+  
+  if (phoneNumArr[0]=="1"&&phoneNumArr[1]==" "||phoneNumArr[1]=="("){
+    
+  }
+  else if(phoneNumArr.length==10){
+    
+  }
+  else if(){
+          
+  }
+  else{
+  
+  }
+  return false;
 }
