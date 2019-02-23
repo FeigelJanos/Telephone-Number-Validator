92bb008d2fe4d8f35a3c67f004b33faeeefe86db
John Feigel <32643682+FeigelJanos@users.noreply.github.com>
2019-02-23 12:50:23 +0100

    Update TelCheck.js

--- TelCheck.js
+++ TelCheck.js
@@ -1,34 +1,55 @@
 function telephoneCheck(str) {
   let phoneNum=str;
-  let answerArr=[];
-  let answer="";
   let pattern1=/^(1)( |\()(\()?\d{3}(\))?( |\)|-)\d{3}( |-)\d{4}$/;
   let pattern2=/\d{10}$/;
   let pattern3=/^(\()?\d{3}(\))?(\s|-)?\d{3}(\s|-)\d{4}$/;
-  
+  let counter=0;
+
 const phoneNumArr=str.split("");
+
+for(let i=0; i<phoneNumArr.length; i++){
+  if(phoneNumArr[i]=="("||phoneNumArr[i]==")"){
+    counter++;
+  }
+}
+  if(counter>0&&counter%2!=0){
+    return false;
+  }
   
-  if (phoneNumArr[0]=="1"&&phoneNumArr[1]==" "||phoneNumArr[1]=="("){
-    answerArr=phoneNum.match(pattern1);
-    answer=answerArr.join("");
-   if(answer==phoneNum){
+  else if (phoneNumArr[0]=="1"&&phoneNumArr[1]==" "||phoneNumArr[1]=="("){
+  
+   if(pattern1.test(phoneNum)){
+  
      return true;             
      }
-    
+    else{
+     
+      return false;
+    }
   }
-  else if(phoneNumArr.length==10){
-    answerArr=phoneNum.match(pattern2);
-    answer=answerArr.join("");
-   if(answer==phoneNum){
+
+   else if(phoneNumArr.length==10){
+   
+   if(pattern2.test(phoneNum)){
+
      return true;             
      }
+     else{
+
+       return false;
+     }
   }
+else if(phoneNumArr[0]!="1"&&phoneNumArr.length>10){
+ 
+   if(pattern3.test(phoneNum)){
+     return true;             
+     }
+     else {
+       return false;
+     }
+  }
+
   else{
-  answerArr=phoneNum.match(pattern3);
-    answer=answerArr.join("");
-   if(answer==phoneNum){
-     return true;             
-     }
+    return false;
   }
-  return false;
 }
